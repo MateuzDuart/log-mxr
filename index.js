c77e4aefa06bf5adbbc5379d6bc8d3dd20b32ec8
MateuzDuart <ltbmateus@gmail.com>
2023-05-31 13:07:45 -0300

    aplicação do css

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const body = document.querySelector('body')
+const container = document.querySelector('.container')
 
 fetch('./log.json').then(response => response.json())
 .then(data => {
 
     Object.entries(data).map((dado) => {
         console.log(dado)
-        const div = document.createElement('div') 
+        const div = document.createElement('div')
+        div.classList.add('containerDia')
         const h2 = document.createElement('h2')
         h2.innerHTML = `${dado[0]}`
         div.appendChild(h2)
         
         Object.entries(dado[1]).map(([chave, valor]) => {
             const p = document.createElement('p')
-            p.innerHTML = `${chave}: ${valor}`
+            p.innerHTML = `<div class="textcontainer"><span class="bold">${chave}:</span> ${valor}</div>`
+            if (chave === 'veja minha tela' && valor !== 'não ativo no momento') {
+            p.innerHTML = `<div class="textcontainer"><span class="bold">${chave}:</span> <a href="${valor}">Me acompanhe ao vivo</a></div>`
+
+            }
             div.appendChild(p)
         })
 
-        body.appendChild(div)
+        container.appendChild(div)
         
     })
     
